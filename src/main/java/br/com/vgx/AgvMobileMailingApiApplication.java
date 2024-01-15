7e4057078e4ea53ceace90524d91a675502b28af
Taciano Fonseca <tacianolgf@gmail.com>
2024-01-15 14:37:22 -0300

    start project

---
+++ src/main/java/br/com/vgx/AgvMobileMailingApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vgx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgvMobileMailingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgvMobileMailingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vgx/AgvMobileMailingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vgx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgvMobileMailingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
